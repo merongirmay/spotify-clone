65041318227f787f0d3336286794b57cc6d38a29
Tomasz Pietraszek <tomekpietraszek@gmail.com>
2023-11-07 21:30:55 +0100

    control button working

--- src/components/PlayerControls.jsx
+++ src/components/PlayerControls.jsx
@@ -2,8 +2,71 @@ import styled from "styled-components";
 import { FaPlayCircle } from "react-icons/fa";
 import { CiRepeat } from "react-icons/ci";
 import { BiSkipNext, BiSkipPrevious, BiShuffle } from "react-icons/bi";
+import { BsFillPlayCircleFill, BsFillPauseCircleFill } from "react-icons/bs";
+import { reducerCases } from "../utils/Constants";
+import axios from "axios";
+import { useStateProvider } from "../utils/StateProvider";
 
 function PlayerControls() {
+  const [{ token, playerState }, dispatch] = useStateProvider();
+
+  const changeTrack = async (type) => {
+    await axios.post(
+      `https://api.spotify.com/v1/me/player/${type}`,
+      {},
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const response = await axios.get(
+      "https://api.spotify.com/v1/me/player/currently-playing",
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    const { item } = data;
+    if (data !== "") {
+      const currentlyPlaying = {
+        id: item.id,
+        name: item.name,
+        artists: item.artists.map((artist) => artist.name),
+        image: item.album.images[2].url,
+      };
+      dispatch({ type: reducerCases.SET_CURRENTLY_PLAYING, currentlyPlaying });
+    } else {
+      dispatch({
+        type: reducerCases.SET_CURRENTLY_PLAYING,
+        currentlyPlaying: null,
+      });
+    }
+  };
+
+  const changeState = async () => {
+    const state = playerState ? "pause" : "play";
+
+    const response = await axios.put(
+      `https://api.spotify.com/v1/me/player/${state}`,
+      {},
+      {
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    dispatch({
+      type: reducerCases.SET_PLAYER_STATE,
+      playerState: !playerState,
+    });
+  };
+
   return (
     <Container>
       <div className="left-controls">
@@ -11,13 +74,17 @@ function PlayerControls() {
           <BiShuffle />
         </div>
         <div className="previous">
-          <BiSkipPrevious />
+          <BiSkipPrevious onClick={() => changeTrack("previous")} />
         </div>
         <div className="play">
-          <FaPlayCircle />
+        {playerState ? (
+          <BsFillPauseCircleFill onClick={changeState} />
+        ) : (
+          <BsFillPlayCircleFill onClick={changeState} />
+        )}
         </div>
         <div className="next">
-          <BiSkipNext />
+          <BiSkipNext onClick={() => changeTrack("next")} />
         </div>
         <div className="repeat">
           <CiRepeat />
