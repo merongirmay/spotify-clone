74f63e03f125c15b65d8ec6e8ed208ac3f6ec87c
Meron <merongirmay59@gmailcom>
2023-11-07 13:29:13 +0100

    worked on body

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ const Container = styled.div`
   color: white;
   height: 100%;
   width: 100%;
-  border-top: 1px solid white;
+  /* border-top: 1px solid white; */
   display: grid;
     grid-template-columns: 1fr 2fr 1fr;
   /* display: flex; */
